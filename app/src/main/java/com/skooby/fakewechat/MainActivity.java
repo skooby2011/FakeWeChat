ce0b1b3df42805baffa28cbaa13ae99985ad27ce
skooby <skooby2011@126.com>
2016-06-28 07:53:27 +0800

    add resource file

---
+++ app/src/main/java/com/skooby/fakewechat/MainActivity.java
@@ -0,0 +1,13 @@
+package com.skooby.fakewechat;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
